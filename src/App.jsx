15c2a8f740496a90b3d063a5c7940dc735ce0799
Cameron <cameronbackler4981@icloud.com>
2025-07-01 21:05:08 +0100

    Initial commit - setup Vite and React.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to 🎸 Guitar Tuner 🎸</h1>
+      <p>Let's tune your guitar!</p>
+    </div>
+  );
+}
+
+export default App;
